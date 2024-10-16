c3284d6af9c098af5ccaf0449bcb2c376ffab5f2
AVerster07 <averster07@gmail.com>
2024-10-16 17:52:16 +0200

    Arie made changes to Featured Listing cards, nav bars, sell page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import { Link, redirect } from 'react-router-dom'
-import Cookie from "js-cookie"
-import serverUrl from '../serverUrl'
+import React, { useEffect, useState } from "react";
+import { Link, redirect } from "react-router-dom";
+import Cookie from "js-cookie";
+import serverUrl from "../serverUrl";
 const Navbar = () => {
-  const [user,setUser] = useState(false)
-  const [logout,setLogout] = useState(false)
+  const [user, setUser] = useState(false);
+  const [logout, setLogout] = useState(false);
 
-  useEffect(()=>{
-    if(Cookie.get("user")){
-      setUser(JSON.parse(Cookie.get("user")))
+  useEffect(() => {
+    if (Cookie.get("user")) {
+      setUser(JSON.parse(Cookie.get("user")));
     }
-  },[])
+  }, []);
 
-  function handleHover(e){
+  function handleHover(e) {
     if (logout) {
-      setLogout(false)
-    }else{
-      setLogout(true)
+      setLogout(false);
+    } else {
+      setLogout(true);
     }
   }
 
-  function handleLogout(e){
-    Cookie.remove("user")
-    window.location.reload()
+  function handleLogout(e) {
+    Cookie.remove("user");
+    window.location.reload();
   }
   return (
-    <div className='Navbar'>
-        <nav class="navbar">
+    <div className="Navbar">
+      <nav class="navbar">
         <div class="logo">
           <a href="index.html" class="logo">
             <img
               href="index.html"
-              src={serverUrl+"/image/logo2.png"}
+              src={serverUrl + "/image/logo2.png"}
               alt="RealHome Logo"
             />
           </a>
         </div>
         <ul class="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/buy">Buy</Link></li>
-          <li><Link to="/rent">Rent</Link></li>
-          {user? 
-          <><li><Link to="/sell">Sell</Link></li></>:
-          <></>}
-          
-          <li><Link to="/agents">Agents</Link></li>
-          <li><Link to="/favourites">Favourites</Link></li>
-          {user ? 
-          <div onMouseEnter={handleHover} onMouseLeave={handleHover}><li><a><span>Hello {user.name}</span></a></li>
-          {logout? <><div className='logout' onClick={handleLogout}>Logout</div></>:<></>}
-          </div>:
-          <><li><Link to="/login">Login</Link></li></>}
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/buy">Buy</Link>
+          </li>
+          <li>
+            <Link to="/rent">Rent</Link>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <Link to="/sell">List</Link>
+              </li>
+            </>
+          ) : (
+            <></>
+          )}
+
+          <li>
+            <Link to="/agents">Agents</Link>
+          </li>
+          <li>
+            <Link to="/favourites">Favourites</Link>
+          </li>
+          {user ? (
+            <div onMouseEnter={handleHover} onMouseLeave={handleHover}>
+              <li>
+                <a>
+                  <span>Hello {user.name}</span>
+                </a>
+              </li>
+              {logout ? (
+                <>
+                  <div className="logout" onClick={handleLogout}>
+                    Logout
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,191 +1,233 @@
-import React,{useEffect,useState} from 'react'
-import Navbar from '../components/Navbar'
-import Cookie from "js-cookie"
-import serverUrl from '../serverUrl'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Cookie from "js-cookie";
+import serverUrl from "../serverUrl";
 const Sell = () => {
+  const [userId, setUserId] = useState(null);
 
-  const [userId,setUserId] = useState(null)
-
-  useEffect(()=>{
+  useEffect(() => {
     try {
-      setUserId(JSON.parse(Cookie.get("user"))._id)
+      setUserId(JSON.parse(Cookie.get("user"))._id);
       console.log(JSON.parse(Cookie.get("user"))._id);
     } catch (error) {
       console.log("User not logged in");
     }
-
-  })
+  });
   return (
-    
-    <div className='Sell'>
-      <Navbar/>
+    <div className="Sell">
+      <Navbar />
       <section class="sell-property">
-      <h2>List Your <span>Property</span></h2>
-      <form
-        id="property-form"
-        action={serverUrl+"/list-property"}
-        method="post"
-        enctype="multipart/form-data"
-      >
-      <input type="text" hidden="true" name='userId' value={userId}  />
-        <div class="form-section">
-          <h3>Property Details</h3>
-          <label for="listingType">Listing Type:</label>
-          <select id="listingType" name="listingType">
-            <option value="buy" name="listingType">Sale</option>
-            <option value="rent" name="listingType">Rental</option>
-          </select>
+        <h2>
+          List Your <span>Property</span>
+        </h2>
+        <form
+          id="property-form"
+          action={serverUrl + "/list-property"}
+          method="post"
+          enctype="multipart/form-data"
+        >
+          <input type="text" hidden="true" name="userId" value={userId} />
+          <div class="form-section">
+            <h3>Property Details</h3>
+            <label for="listingType">Listing Type:</label>
+            <select id="listingType" name="listingType">
+              <option value="buy" name="listingType">
+                Sale
+              </option>
+              <option value="rent" name="listingType">
+                Rental
+              </option>
+            </select>
 
-          <label for="propertyType">Property Type:</label>
-          <select id="propertyType" name="propertyType">
-            <option value="House">House</option>
-            <option value="Apartment">Apartment</option>
-            <option value="Townhouse">Townhouse</option>
-          </select>
+            <label for="propertyType">Property Type:</label>
+            <select id="propertyType" name="propertyType">
+              <option value="House">House</option>
+              <option value="Apartment">Apartment</option>
+              <option value="Townhouse">Townhouse</option>
+            </select>
 
-          <label for="erfSize">Erf Size (m²):</label>
-          <input type="number" id="erfSize" name="erfSize" required />
+            <label for="erfSize">Erf Size (m²):</label>
+            <input type="number" id="erfSize" name="erfSize" required />
 
-          <label for="address">Address:</label>
-          <input type="text" id="address" name="address" required />
+            <label for="address">Address:</label>
+            <input type="text" id="address" name="address" required />
 
-          <label for="suburb">Suburb:</label>
-          <input type="text" id="suburb" name="suburb" required />
+            <label for="city">City:</label>
+            <input type="text" id="city" name="city" required />
 
-          <label for="city">City:</label>
-          <input type="text" id="city" name="city" required />
+            <label for="suburb">Suburb:</label>
+            <input type="text" id="suburb" name="suburb" required />
 
-          <label for="province">Province:</label>
-          <input type="text" id="province" name="province" required />
-        </div>
-
-        <div class="form-section">
-          <h3>Contacts</h3>
-          <label for="sellerName">Seller/Landlord Name:</label>
-          <input type="text" id="sellerName" name="sellerName" required />
-
-          <label for="sellerEmail">Seller/Landlord Email:</label>
-          <input type="email" id="sellerEmail" name="sellerEmail" required />
-
-          <label for="seller-mobile">Seller/Landlord Mobile Number:</label>
-          <input type="tel" id="sellerMobile" name="sellerMobile" required />
-
-          <label for="agent-name">Listing Agent Name:</label>
-          <input type="text" id="agent-name" name="agentName" />
-
-          <label for="agent-email">Listing Agent Email:</label>
-          <input type="email" id="agent-email" name="agentEmail" />
-
-          <label for="agent-mobile">Listing Agent Mobile Number:</label>
-          <input type="tel" id="agent-mobile" name="agentMobile" />
-        </div>
-
-        <div class="form-section">
-          <h3>Marketing</h3>
-          <label for="list-price">List Price (R):</label>
-          <input type="number" id="list-price" name="listPrice" required />
-
-          <label for="rates">Rates (R):</label>
-          <input type="number" id="rates" name="rates" />
-
-          <label for="levies">Levies (R):</label>
-          <input type="number" id="levies" name="levies" />
-
-          <label for="listing-heading">Listing Heading:</label>
-          <input
-            type="text"
-            id="listing-heading"
-            name="listingHeading"
-            required
-          />
-
-          <label for="listing-description">Listing Description:</label>
-          <textarea
-            id="listing-description"
-            name="listingDescription"
-            rows="5"
-            required
-          ></textarea>
-
-          <label for="property-images">Upload Images:</label>
-          <input
-            type="file"
-            id="property-images"
-            name="property-images"
-            multiple
-          />
-        </div>
-
-        <div class="form-section">
-          <h3>Features</h3>
-          <label for="pets-allowed">Pets Allowed:</label>
-          <select id="petsAllowed" name="petsAllowed">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-
-          <label for="bedrooms">Bedrooms:</label>
-          <input type="number" id="bedrooms" name="bedrooms" />
-
-          <label for="bathrooms">Bathrooms:</label>
-          <input type="number" id="bathrooms" name="bathrooms" />
-
-          <label for="carports">Carports:</label>
-          <input type="number" id="carports" name="carports" />
-
-          <div className="additional-features">
-            
-          <label for="kitchen">Kitchen:</label>
-          <input type="checkbox" id="kitchen" name="kitchen" value="Kitchen" />
-
-          <label for="living-room">Living Room:</label>
-          <input
-            type="checkbox"
-            id="living-room"
-            name="features"
-            value="Living Room"
-          />
-
-          <label for="study">Study/Office:</label>
-          <input
-            type="checkbox"
-            id="study"
-            name="features"
-            value="Study/Office"
-          />
-
-          <label for="garage">Garage:</label>
-          <input type="checkbox" id="garage" name="features" value="Garage" />
-
-          <label for="carport">Carport:</label>
-          <input type="checkbox" id="carport" name="features" value="Carport" />
-
-          <label for="garden">Garden:</label>
-          <input type="checkbox" id="garden" name="features" value="Garden" />
-
-          <label for="pool">Pool:</label>
-          <input type="checkbox" id="pool" name="features" value="Pool" />
-
-          <label for="flatlet">Flatlet:</label>
-          <input type="checkbox" id="flatlet" name="features" value="Flatlet" />
+            <label for="province">Province:</label>
+            <input type="text" id="province" name="province" required />
           </div>
-        </div>
 
-        <div class="form-section">
-          <h3>Additional Features</h3>
-          <textarea
-            id="additional-features"
-            name="additionalFeatures"
-            rows="3"
-            placeholder="Add any additional features"
-          ></textarea>
-        </div>
-        
-        <button type="submit">Submit Property</button>
-      </form>
-    </section>
+          <div class="form-section">
+            <h3>Contacts</h3>
+            <label for="sellerName">Seller/Landlord Name:</label>
+            <input type="text" id="sellerName" name="sellerName" required />
+
+            <label for="sellerEmail">Seller/Landlord Email:</label>
+            <input type="email" id="sellerEmail" name="sellerEmail" required />
+
+            <label for="seller-mobile">Seller/Landlord Mobile Number:</label>
+            <input type="tel" id="sellerMobile" name="sellerMobile" required />
+
+            <label for="agent-name">Listing Agent Name:</label>
+            <input type="text" id="agent-name" name="agentName" />
+
+            <label for="agent-email">Listing Agent Email:</label>
+            <input type="email" id="agent-email" name="agentEmail" />
+
+            <label for="agent-mobile">Listing Agent Mobile Number:</label>
+            <input type="tel" id="agent-mobile" name="agentMobile" />
+          </div>
+
+          <div class="form-section">
+            <h3>Marketing</h3>
+            <label for="list-price">List Price (R):</label>
+            <input type="number" id="list-price" name="listPrice" required />
+
+            <label for="rates">Rates (R):</label>
+            <input type="number" id="rates" name="rates" />
+
+            <label for="levies">Levies (R):</label>
+            <input type="number" id="levies" name="levies" />
+
+            <label for="listing-heading">Listing Heading:</label>
+            <input
+              type="text"
+              id="listing-heading"
+              name="listingHeading"
+              required
+            />
+
+            <label for="listing-description">Listing Description:</label>
+            <textarea
+              id="listing-description"
+              name="listingDescription"
+              rows="5"
+              required
+            ></textarea>
+
+            <label for="property-images">Upload Images:</label>
+            <input
+              type="file"
+              id="property-images"
+              name="property-images"
+              multiple
+            />
+          </div>
+
+          <div class="form-section">
+            <h3>Features</h3>
+            <label for="pets-allowed">Pets Allowed:</label>
+            <select id="petsAllowed" name="petsAllowed">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+
+            <label for="bedrooms">Bedrooms:</label>
+            <input
+              type="number"
+              id="bedrooms"
+              name="bedrooms"
+              placeholder="Amount of bedrooms"
+            />
+
+            <label for="bathrooms">Bathrooms:</label>
+            <input
+              type="number"
+              id="bathrooms"
+              name="bathrooms"
+              placeholder="Amount of bathrooms"
+            />
+
+            <label for="carports">Garages:</label>
+            <input
+              type="number"
+              id="carports"
+              name="carports"
+              placeholder="2 if double garage"
+            />
+
+            <div className="additional-features">
+              <label for="kitchen">Kitchen:</label>
+              <input
+                type="checkbox"
+                id="kitchen"
+                name="kitchen"
+                value="Kitchen"
+              />
+
+              <label for="living-room">Living Room:</label>
+              <input
+                type="checkbox"
+                id="living-room"
+                name="features"
+                value="Living Room"
+              />
+
+              <label for="study">Study/Office:</label>
+              <input
+                type="checkbox"
+                id="study"
+                name="features"
+                value="Study/Office"
+              />
+
+              {/* <label for="garage">Garage:</label>
+              <input
+                type="checkbox"
+                id="garage"
+                name="features"
+                value="Garage"
+              /> */}
+
+              <label for="carport">Carport:</label>
+              <input
+                type="checkbox"
+                id="carport"
+                name="features"
+                value="Carport"
+              />
+
+              <label for="garden">Garden:</label>
+              <input
+                type="checkbox"
+                id="garden"
+                name="features"
+                value="Garden"
+              />
+
+              <label for="pool">Pool:</label>
+              <input type="checkbox" id="pool" name="features" value="Pool" />
+
+              <label for="flatlet">Flatlet:</label>
+              <input
+                type="checkbox"
+                id="flatlet"
+                name="features"
+                value="Flatlet"
+              />
+            </div>
+          </div>
+
+          <div class="form-section">
+            <h3>Additional Features</h3>
+            <textarea
+              id="additional-features"
+              name="additionalFeatures"
+              rows="3"
+              placeholder="Add any additional features"
+            ></textarea>
+          </div>
+
+          <button type="submit">Submit Property</button>
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Sell
+export default Sell;
